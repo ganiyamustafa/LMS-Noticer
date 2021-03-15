10220a29209809e48bb43cccfaa3608f4ba90d62
GaniyaMustafa <ganiyamustaga32@gmail.com>
2021-03-15 12:42:06 +0700

    fix some bugs

--- app.py
+++ app.py
@@ -1,86 +1,86 @@
-# import datetime
-# import os
-# from mailer import Email
-# from mailing_list import MailingList
-# import schedule
-# from lms import LMS
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-# from json_handler import load_data
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# import time
+import datetime
+import os
+from mailer import Email
+from mailing_list import MailingList
+import schedule
+from lms import LMS
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from json_handler import load_data
+from apscheduler.schedulers.blocking import BlockingScheduler
+import time
 
-# def generate_email_body(lms:LMS, email_sender:str, email_receiver:list, course_name, body):
-#     try:
-#         body_message = html_body = ''
-#         receivers = ', '.join(receiver for receiver in email_receiver)
-#         message = MIMEMultipart("alternative")
-#         message["Subject"] = f'{course_name} Baru Saja Memposting Tugas Baru'
-#         message["From"] = email_sender
-#         message["To"] = receivers
+def generate_email_body(lms:LMS, email_sender:str, email_receiver:list, course_name, body):
+    try:
+        body_message = html_body = ''
+        receivers = ', '.join(receiver for receiver in email_receiver)
+        message = MIMEMultipart("alternative")
+        message["Subject"] = f'{course_name} Baru Saja Memposting Tugas Baru'
+        message["From"] = email_sender
+        message["To"] = receivers
 
-#         body = body
-#         data = load_data(f'Course Data/XII RPL A/{course_name}.json')
+        body = body
+        data = load_data(f'Course Data/XII RPL A/{course_name}.json')
 
-#         for link in body:
-#             body_message += f'\n{link}'
-#             try:
-#                 title = [d['Title'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link'] and link in d['Link']]
+        for link in body:
+            body_message += f'\n{link}'
+            try:
+                title = [d['Title'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link'] and link in d['Link']]
 
-#             except Exception as e:
-#                 title = '#'
+            except Exception as e:
+                title = '#'
 
-#             html_body += f'<li><a href="{link}">{title}</a></li>'
-#         html = f"""
-#             <html>
-#             <body>
-#                 <ul>
-#                     {html_body}
-#                 </ul>
-#             </body>
-#             </html>
-#         """            
-#         part1 = MIMEText(body_message, "plain")
-#         part2 = MIMEText(html, "html")
-#         message.attach(part1)
-#         message.attach(part2)
-#         return message
-#     except Exception as e:
-#         print(e)
-#         return None
+            html_body += f'<li><a href="{link}">{title}</a></li>'
+        html = f"""
+            <html>
+            <body>
+                <ul>
+                    {html_body}
+                </ul>
+            </body>
+            </html>
+        """            
+        part1 = MIMEText(body_message, "plain")
+        part2 = MIMEText(html, "html")
+        message.attach(part1)
+        message.attach(part2)
+        return message
+    except Exception as e:
+        print(e)
+        return None
 
-# def check_if_update(lms:LMS, email:Email, ml:MailingList, course:str, day:str):
-#     info_update = lms.course_updated(course)
-#     if any(info_update):
-#         message = generate_email_body(lms, email.get_user(), ml.get_list(), course, info_update)
-#         for receiver in ml.get_list():
-#             email.server.sendmail(email.get_user(), receiver, message.as_string())
-#     else:
-#         print(f'{day} pukul {datetime.datetime.now().hour} {course} belum ada update')
+def check_if_update(lms:LMS, email:Email, ml:MailingList, course:str, day:str):
+    info_update = lms.course_updated(course)
+    if any(info_update):
+        message = generate_email_body(lms, email.get_user(), ml.get_list(), course, info_update)
+        for receiver in ml.get_list():
+            email.server.sendmail(email.get_user(), receiver, message.as_string())
+    else:
+        print(f'{day} pukul {datetime.datetime.now().hour} {course} belum ada update')
 
-# if __name__ == "__main__":
-#     print('BOT STARTING\n')
+if __name__ == "__main__":
+    print('BOT STARTING\n')
 
-#     email = Email('pirus.apl@gmail.com', 'pirus123')
-#     lms = LMS('181113847', 'PRSYETI1')
-#     ml = MailingList()
-#     login_email = False
+    email = Email('pirus.apl@gmail.com', 'pirus123')
+    lms = LMS('181113847', 'PRSYETI1')
+    ml = MailingList()
+    login_email = False
 
-#     while not login_email:
-#         if email.login():
-#             login_email = True
-#             print(f'Email login succes, logged in as {email.get_user()}\n')
-#         else:
-#             print('Email login failed, retrying...\n')
+    while not login_email:
+        if email.login():
+            login_email = True
+            print(f'Email login succes, logged in as {email.get_user()}\n')
+        else:
+            print('Email login failed, retrying...\n')
 
-#     if not os.path.isfile('Course Data/XII RPL A/Course List.json'):
-#         lms.get_courses()
+    if not os.path.isfile('Course Data/XII RPL A/Course List.json'):
+        lms.get_courses()
 
-#     sched = BlockingScheduler()
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'PPKn XII (Tintin Sutrisni)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'Produk Kreatif Kewirausahaan XII (Sopiah)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Indonesia XII (Dra. Hj. Sri Gantini, M.Pd)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Inggris XII (Nurhayati Hutabarat, S.Pd)', 'Rabu'), 'cron', day_of_week='wed', hour='0-15')
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Jepang XII (Rukti Ananditya Karunia Sari, S.Pd.)', 'Jumat'), 'cron', day_of_week='fri', hour='0-15')
-#     sched.add_job(lambda:check_if_update(lms, email, ml, 'PABP XII (LUKMAN)', 'Jumat'), 'cron', day_of_week='fri', hour='0-15')
-#     sched.start()
+    sched = BlockingScheduler()
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'PPKn XII (Tintin Sutrisni)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'Produk Kreatif Kewirausahaan XII (Sopiah)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Indonesia XII (Dra. Hj. Sri Gantini, M.Pd)', 'Selasa'), 'cron', day_of_week='tue', hour='0-15')
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Inggris XII (Nurhayati Hutabarat, S.Pd)', 'Rabu'), 'cron', day_of_week='wed', hour='0-15')
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'Bahasa Jepang XII (Rukti Ananditya Karunia Sari, S.Pd.)', 'Jumat'), 'cron', day_of_week='fri', hour='0-15')
+    sched.add_job(lambda:check_if_update(lms, email, ml, 'PABP XII (LUKMAN)', 'Jumat'), 'cron', day_of_week='fri', hour='0-15')
+    sched.start()
--- lms.py
+++ lms.py
@@ -1,128 +1,128 @@
-# import mechanicalsoup
-# from json_handler import save_data, load_data
-# from threading import Thread
-# import numpy as np
-# import os
+import mechanicalsoup
+from json_handler import save_data, load_data
+from threading import Thread
+import numpy as np
+import os
 
-# class LMS():
-#     base_url = "https://lms.smkn1-cmi.sch.id/"
-#     browser = mechanicalsoup.StatefulBrowser()
-#     browser.open(base_url)
-#     username = ""
-#     password = ""
-#     course_list = 'Course Data/XII RPL A/Course List.json'
+class LMS():
+    base_url = "https://lms.smkn1-cmi.sch.id/"
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open(base_url)
+    username = ""
+    password = ""
+    course_list = 'Course Data/XII RPL A/Course List.json'
 
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-#     def login(self):
-#         if not self.is_logged():
-#             self.browser.follow_link("login/index.php")
-#             self.browser.select_form('form[id="login"]')
-#             self.browser["username"] = self.username
-#             self.browser["password"] = self.password
-#             self.browser.submit_selected()
+    def login(self):
+        if not self.is_logged():
+            self.browser.follow_link("login/index.php")
+            self.browser.select_form('form[id="login"]')
+            self.browser["username"] = self.username
+            self.browser["password"] = self.password
+            self.browser.submit_selected()
 
-#     def is_logged(self):
-#         test_link = 'https://lms.smkn1-cmi.sch.id/my/'
-#         self.browser.open(test_link)
-#         result = self.browser.url == test_link
-#         self.browser.open(self.base_url)
-#         return result
+    def is_logged(self):
+        test_link = 'https://lms.smkn1-cmi.sch.id/my/'
+        self.browser.open(test_link)
+        result = self.browser.url == test_link
+        self.browser.open(self.base_url)
+        return result
 
-#     def page(self):
-#         return self.browser.get_current_page()
+    def page(self):
+        return self.browser.get_current_page()
 
-#     def get_courses(self):
-#         self.login()
-#         courses_list = []
-#         self.browser.open(f"{self.base_url}/my/index.php?mynumber=-2")
+    def get_courses(self):
+        self.login()
+        courses_list = []
+        self.browser.open(f"{self.base_url}/my/index.php?mynumber=-2")
 
-#         for course in self.page().select('.course_list .coursebox .course_title h2'):
-#             courses = {}
-#             courses['Title'] = course.text
-#             courses['Link'] = course.a['href']
-#             courses_list.append(courses)
-#         save_data('Course Data/XII RPL A/', 'Course List.json', courses_list)
+        for course in self.page().select('.course_list .coursebox .course_title h2'):
+            courses = {}
+            courses['Title'] = course.text
+            courses['Link'] = course.a['href']
+            courses_list.append(courses)
+        save_data('Course Data/XII RPL A/', 'Course List.json', courses_list)
 
-#         return courses_list
+        return courses_list
 
-#     def get_assignment_data(self, link, title):
-#         global activity_list, assignments_list, data_list, data
-#         self.browser.open(link)
-#         for assignment_li in self.page().select('li.section.main.clearfix'):
-#             activity = {}
-#             assignments_list = []
-#             if 'General' not in assignment_li['aria-label']:
-#                 activity['Header'] = assignment_li['aria-label']
-#                 for assignment in assignment_li.select('div.content li.activity'):
-#                     assignments = {}
-#                     if 'label' not in assignment['class']:
-#                         if 'assign' in assignment['class']: assignments['Type'] = 'submission/assign'
-#                         if assignment.find('span', class_='accesshide'): assignment.find('span', class_='accesshide').extract()
-#                         if assignment.find('span', class_='instancename'): assignments['Title'] = assignment.find('span', class_='instancename').text
-#                         if assignment.a: assignments['Link'] = assignment.a['href']
-#                         else: assignments['Link'] = None
-#                         assignments_list.append(assignments)
-#                 activity['Activity List'] = assignments_list
+    def get_assignment_data(self, link, title):
+        global activity_list, assignments_list, data_list, data
+        self.browser.open(link)
+        for assignment_li in self.page().select('li.section.main.clearfix'):
+            activity = {}
+            assignments_list = []
+            if 'General' not in assignment_li['aria-label']:
+                activity['Header'] = assignment_li['aria-label']
+                for assignment in assignment_li.select('div.content li.activity'):
+                    assignments = {}
+                    if 'label' not in assignment['class']:
+                        if 'assign' in assignment['class']: assignments['Type'] = 'submission/assign'
+                        if assignment.find('span', class_='accesshide'): assignment.find('span', class_='accesshide').extract()
+                        if assignment.find('span', class_='instancename'): assignments['Title'] = assignment.find('span', class_='instancename').text
+                        if assignment.a: assignments['Link'] = assignment.a['href']
+                        else: assignments['Link'] = None
+                        assignments_list.append(assignments)
+                activity['Activity List'] = assignments_list
                 
-#                 activity_list.append(activity)
+                activity_list.append(activity)
 
-#         data['Course'] = title
-#         data['Activity'] = activity_list
-#         data_list.append(data)
+        data['Course'] = title
+        data['Activity'] = activity_list
+        data_list.append(data)
         
-#     def get_assignment(self, courses: list, course_name: str=''):
-#         global activity_list, assignments_list, data_list, data
-#         data_list = []
-#         threads = []
-#         for course in courses:
-#             data = {}
-#             activity_list = []
-#             assignments_list = []
+    def get_assignment(self, courses: list, course_name: str=''):
+        global activity_list, assignments_list, data_list, data
+        data_list = []
+        threads = []
+        for course in courses:
+            data = {}
+            activity_list = []
+            assignments_list = []
 
-#             if course_name in course['Title']:
-#                 t = Thread(target=self.get_assignment_data, args=(course['Link'],course['Title']))
-#                 threads.append(t)
+            if course_name in course['Title']:
+                t = Thread(target=self.get_assignment_data, args=(course['Link'],course['Title']))
+                threads.append(t)
 
-#         for t in threads:
-#             t.start()
+        for t in threads:
+            t.start()
 
-#         for t in threads:
-#             t.join()
+        for t in threads:
+            t.join()
 
-#         return data_list
+        return data_list
 
-#     def course_updated(self, course:str):
-#         self.login()
+    def course_updated(self, course:str):
+        self.login()
 
-#         if not os.path.isfile(f'Course Data/XII RPL A/{course}.json'):
-#             print(f'Creating Course Data/XII RPL A/{course}.json...')
-#             self.get_course_detail(course)
+        if not os.path.isfile(f'Course Data/XII RPL A/{course}.json'):
+            print(f'Creating Course Data/XII RPL A/{course}.json...')
+            self.get_course_detail(course)
 
-#         old_data = load_data(f'Course Data/XII RPL A/{course}.json')
-#         data = self.get_assignment(load_data(self.course_list), course)
-#         data_copied = data.copy()
+        old_data = load_data(f'Course Data/XII RPL A/{course}.json')
+        data = self.get_assignment(load_data(self.course_list), course)
+        data_copied = data.copy()
 
-#         title = [d['Title'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link']]
-#         data = [d['Link'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link']]
+        title = [d['Title'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link']]
+        data = [d['Link'] for d_ in data[0]['Activity'] for d in d_['Activity List'] if d['Link']]
 
-#         old_title = [d['Title'] for od in old_data[0]['Activity'] for d in od['Activity List'] if d['Link']]
-#         old_data = [d['Link'] for od in old_data[0]['Activity'] for d in od['Activity List'] if d['Link']]
+        old_title = [d['Title'] for od in old_data[0]['Activity'] for d in od['Activity List'] if d['Link']]
+        old_data = [d['Link'] for od in old_data[0]['Activity'] for d in od['Activity List'] if d['Link']]
 
-#         diff = np.setdiff1d(data, old_data)
+        diff = np.setdiff1d(data, old_data)
         
-#         save_data('Course Data/XII RPL A/', f'{course}.json', data_copied)
+        save_data('Course Data/XII RPL A/', f'{course}.json', data_copied)
 
-#         return diff if diff.size > 0 else []
+        return diff if diff.size > 0 else []
 
-#     def get_course_detail(self, course:str):
-#         self.login()
-#         save_data('Course Data/XII RPL A/', f'{course}.json', self.get_assignment(load_data(self.course_list), course))
-#         return load_data(f'Course Data/XII RPL A/{course}.json')
+    def get_course_detail(self, course:str):
+        self.login()
+        save_data('Course Data/XII RPL A/', f'{course}.json', self.get_assignment(load_data(self.course_list), course))
+        return load_data(f'Course Data/XII RPL A/{course}.json')
 
-# # lms = LMS('181113847', 'PRSYETI1')
-# # for course in ['PPKn XII (Tintin Sutrisni)', 'Produk Kreatif Kewirausahaan XII (Sopiah)', 'Bahasa Indonesia XII (Dra. Hj. Sri Gantini, M.Pd)', 'Bahasa Inggris XII (Nurhayati Hutabarat, S.Pd)', 'Bahasa Jepang XII (Rukti Ananditya Karunia Sari, S.Pd.)', 'PABP XII (LUKMAN)']:
-# #     print(course)
-# #     lms.get_course_detail(course)
+# lms = LMS('181113847', 'PRSYETI1')
+# for course in ['PPKn XII (Tintin Sutrisni)', 'Produk Kreatif Kewirausahaan XII (Sopiah)', 'Bahasa Indonesia XII (Dra. Hj. Sri Gantini, M.Pd)', 'Bahasa Inggris XII (Nurhayati Hutabarat, S.Pd)', 'Bahasa Jepang XII (Rukti Ananditya Karunia Sari, S.Pd.)', 'PABP XII (LUKMAN)']:
+#     print(course)
+#     lms.get_course_detail(course)
